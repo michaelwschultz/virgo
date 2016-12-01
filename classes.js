28cabc756bb9c2f8c02670cfdefec9707ea4b036
Michael Schultz <michaelwschultz@gmail.com>
2016-12-01 00:21:41 -0800

    Updates shape class to include movement patterns, sequences and types.

--- classes.js
+++ classes.js
@@ -2,25 +2,65 @@
 
 // shape reqires an array of objects that include location.row, .column and color
 class Shape {
-    constructor(shapeType, location, movementPattern) {
+    constructor(shapeType, location) {
       this.shapeType = shapeType;
       this.location = location;
-      this.movementPattern = movementPattern;
     }
 
-    placeShape() {
-        for (let i = 0; i < this.shapeType.length; i++) {
-            turnOn(new Location(this.shapeType[i].row + this.location.row, this.shapeType[i].column + this.location.column), this.shapeType[i].color);
+    // loop = 0 means infinite, anything else will only loop that many times.
+    moveShape(movementSequence, movementType = {type: "loop"}, speed = 30) {
+        let i = 0;
+        let movementPattern = [];
+
+        // combine movement patterns into single array
+        for (let j = 0; j < movementSequence.length; j++) {
+            movementPattern = movementPattern.concat(movementSequence[j]);
         }
-    }
 
-    // TODO: add ability to move shape via movementPattern
-    moveShape() {
-        // setTimeout(function() {
-        //     for (let i = 0; i < this.movementPattern.length; i++) {
-        //         this.location = this.movementPattern;
-        //     }
-        // }, 3000);
+        let rowSteps = movementPattern[i].row;
+        let columnSteps = movementPattern[i].column;
+
+        let move = setInterval(function() {
+            
+            if (columnSteps == 0 && rowSteps == 0) {
+
+                // move to next item in movementPattern
+                i++;
+
+                // has the hole pattern completed?
+                if (i == movementPattern.length) {
+
+                    let numberOfRepeats = movementType.number - 1;
+                    if (movementType.type == "repeat" && numberOfRepeats !== 0) {
+                        // reset i
+                        i = 0;
+                        this.moveShape(movementPattern, {type: "repeat", number: numberOfRepeats}, speed);
+                    }
+
+                    if (movementType.type == "loop") {
+                        // reset i
+                        i = 0;
+                        this.moveShape(movementPattern, movementType, speed);
+                    }
+
+                    clearInterval(move);
+                    return;
+                }
+
+                rowSteps = movementPattern[i].row;
+                columnSteps = movementPattern[i].column;
+            }
+                
+            if (rowSteps !== 0) {
+                this.location.row += rowSteps / Math.abs(rowSteps);
+                rowSteps -= rowSteps / Math.abs(rowSteps);
+            }
+        
+            if (columnSteps !== 0) {
+                this.location.column += columnSteps / Math.abs(columnSteps);
+                columnSteps -= columnSteps / Math.abs(columnSteps);
+            }
+        }.bind(this), speed);
     }
 }
 
