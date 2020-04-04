0311cdb1db99daa8d3df4d0ec36466ac8469fb6e
Michael Schultz <michaelwschultz@gmail.com>
2020-04-04 13:28:48 -0700

    Breaks shapes into a more organized folder structure

---
+++ js/shapes/alphabet.js
@@ -0,0 +1,15 @@
+/* Alphabet */
+export const letterA = [
+  { row: 0, colum: 1, color: "bg-purple" },
+  { row: 0, colum: 2, color: "bg-purple" },
+  { row: 1, colum: 0, color: "bg-purple" },
+  { row: 1, colum: 3, color: "bg-purple" },
+  { row: 2, colum: 0, color: "bg-purple" },
+  { row: 2, colum: 1, color: "bg-purple" },
+  { row: 2, colum: 2, color: "bg-purple" },
+  { row: 2, colum: 3, color: "bg-purple" },
+  { row: 3, colum: 0, color: "bg-purple" },
+  { row: 3, colum: 3, color: "bg-purple" },
+  { row: 4, colum: 0, color: "bg-purple" },
+  { row: 4, colum: 3, color: "bg-purple" },
+]
---
+++ js/shapes/index.js
@@ -0,0 +1,49 @@
+//** Shapes **//
+
+export {
+  letterA,
+  // letterB,
+  // letterC,
+  // letterD,
+  // letterE,
+  // letterF,
+  // letterG,
+  // letterH,
+  // letterI,
+  // letterJ,
+  // letterK,
+  // letterL,
+  // letterM,
+  // letterN,
+  // letterO,
+  // letterP,
+  // letterQ,
+  // letterR,
+  // letterS,
+  // letterT,
+  // letterU,
+  // letterV,
+  // letterW,
+  // letterX,
+  // letterY,
+  // letterZ,
+} from './alphabet.js';
+
+export {
+  largeEnemyShip,
+  smallEnemyShip,
+  userShip,
+} from './ships.js';
+
+export {
+  crown,
+  gameOVer,
+} from './props.js';
+
+export {
+  pattern1,
+  pattern2,
+  patternDropdown,
+  patternScroll,
+  patternStatic,
+} from './patterns.js';
